380a14d277547d5ffe253a5a95e48a3c0e2ff8a9
Carlos Ramos <rmscrlos@gmail.com>
2021-02-16 22:05:11 -0500

    added logger and validateuserid middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,52 @@
-function logger(req, res, next) {
-  // DO YOUR MAGIC
-}
+const usersdb = require('../users/users-model');
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-}
+const logger = format => (req, res, next) => {
+	// DO YOUR MAGIC
+	const time = new Date().toISOString();
+	if (format === 'long') {
+		console.log(`${time} || ${req.method} || ${req.ip} || ${req.url} || ${req.socket.bytesRead} `);
+	} else if (format === 'short') {
+		console.log(`${time} || ${req.methog} || ${req.url}`);
+	} else {
+		console.log(`${time} || ${req.method} || ${req.url} || ${req.socket.bytesRead}`);
+	}
+	next();
+};
+
+const validateUserId = (req, res, next) => {
+	// DO YOUR MAGIC
+	usersdb
+		.getById(req.params.id)
+		.then(user => {
+			if (user) {
+				req.user = user;
+				next();
+			} else {
+				res.status(404).json({
+					message: 'User not found'
+				});
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				message: 'Error retrieving the user.'
+			});
+		});
+};
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+	logger,
+	validateUserId,
+	validateUser,
+	validatePost
+};
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,41 +1,59 @@
 const express = require('express');
-
+const { whereNotExists } = require('../../data/db-config');
+const usersdb = require('./users-model');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', (req, res, next) => {
+	// RETURN AN ARRAY WITH ALL THE USERS
+	usersdb
+		.get()
+		.then(users => {
+			res.status(200).json(users);
+		})
+		.catch(next);
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+router.get('/:id', (req, res, next) => {
+	// RETURN THE USER OBJECT
+	// this needs a middleware to verify user id
+	usersdb
+		.getById(req.params.id)
+		.then(user => {
+			user
+				? res.status(200).json(user)
+				: res.status(404).json({
+						message: 'Could not find user.'
+				  });
+		})
+		.catch(next);
 });
 
 router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+	// RETURN THE NEWLY CREATED USER OBJECT
+	// this needs a middleware to check that the request body is valid
 });
 
 router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+	// RETURN THE FRESHLY UPDATED USER OBJECT
+	// this needs a middleware to verify user id
+	// and another middleware to check that the request body is valid
 });
 
 router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+	// RETURN THE FRESHLY DELETED USER OBJECT
+	// this needs a middleware to verify user id
 });
 
 router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+	// RETURN THE ARRAY OF USER POSTS
+	// this needs a middleware to verify user id
 });
 
 router.post('/:id/posts', (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+	// RETURN THE NEWLY CREATED USER POST
+	// this needs a middleware to verify user id
+	// and another middleware to check that the request body is valid
 });
 
 // do not forget to export the router
+module.exports = router;
